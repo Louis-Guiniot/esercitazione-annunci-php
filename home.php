b7931a3aa98003410718447ae884903277bca33c
Louis-Guiniot <louis99679@gmail.com>
2021-03-03 17:38:24 +0100

    create card bene

--- home.php
+++ home.php
@@ -22,72 +22,70 @@
 
 </head>
 
-<body>
+<body style="background-color:rgba(51, 58, 65, 0.445);">
 
     <div class="box">
-
-    <div class="row">    
-                <div class="col">
+        <div class="row">    
+                    <div class="col">
+                        <div class="input-group mb-3">
+                            <a href="logout.php"><button class="btn btn-dark">logout</button></a>
+                        </div>                
+                    </div>
+                    <div class="col-10">
                     <div class="input-group mb-3">
-                        <a href="logout.php"><button class="btn btn-dark">logout</button></a>
-                    </div>                
-                </div>
-                <div class="col-10">
-                <div class="input-group mb-3">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text" id="inputGroupFileAddon01">Search</span>
-                            </div>
-                            <input type="text" class="form-control" placeholder="insert word to search" name="word" autocomplete="off">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text" id="inputGroupFileAddon01">Search</span>
+                                </div>
+                                <input type="text" class="form-control" placeholder="insert word to search" name="word" autocomplete="off">
 
+                            </div>
+                    </div>
+                </div>
+
+            <form action="upload.php" method="post" enctype="multipart/form-data">
+
+            
+            
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="inputGroupFileAddon01">Upload</span>
                         </div>
-                </div>
-            </div>
-
-        <form action="upload.php" method="post" enctype="multipart/form-data">
-
-        
-        
-                <div class="input-group mb-3">
-                    <div class="input-group-prepend">
-                        <span class="input-group-text" id="inputGroupFileAddon01">Upload</span>
-                    </div>
-                    <div class="custom-file">
-                        <input type="file" class="custom-file-input" id="fileUrl" aria-describedby="inputGroupFileAddon01" name="fileUrl">
-                        <label class="custom-file-label" for="inputGroupFile01">Choose file</label>
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col">
-                        <div class="input-group mb-3">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text" id="inputGroupFileAddon01">Prezzo</span>
-                            </div>
-                            <input type="text" class="form-control" placeholder="prezzo" aria-label="Recipient's username" aria-describedby="button-addon2" name="prezzo" autocomplete="off">
-
+                        <div class="custom-file">
+                            <input type="file" class="custom-file-input" id="fileUrl" aria-describedby="inputGroupFileAddon01" name="fileUrl">
+                            <label class="custom-file-label" for="inputGroupFile01">Choose file</label>
                         </div>
                     </div>
-                    <div class="col">
-                        <div class="input-group mb-3">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text" id="inputGroupFileAddon01">Descrizione</span>
-                            </div>
-                            <textarea type="text" class="form-control" placeholder="prezzo" aria-label="Recipient's username" aria-describedby="button-addon2" name="descrizione" autocomplete="off"></textarea>
 
+                    <div class="row">
+                        <div class="col">
+                            <div class="input-group mb-3">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text" id="inputGroupFileAddon01">Prezzo</span>
+                                </div>
+                                <input type="text" class="form-control" placeholder="prezzo" aria-label="Recipient's username" aria-describedby="button-addon2" name="prezzo" autocomplete="off">
+
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="input-group mb-3">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text" id="inputGroupFileAddon01">Descrizione</span>
+                                </div>
+                                <textarea type="text" class="form-control" placeholder="prezzo" aria-label="Recipient's username" aria-describedby="button-addon2" name="descrizione" autocomplete="off"></textarea>
+
+                            </div>
                         </div>
                     </div>
-                </div>
 
-                <div class="input-group mb-3">
-                    <button type="submit" class="form-control">inserisci</button>
-                </div>
+                    <div class="input-group mb-3">
+                        <button type="submit" class="form-control">inserisci</button>
+                    </div>
 
-        </form>
-
+            </form>
+        </div>
     </div>
 
-
-            <?php
+    <?php
 
             require_once('config.php');
            
@@ -97,23 +95,36 @@
             while($row=$resultSelect->fetch_assoc()){
 
                  echo "
-                <div class=card style='background-color:red; color:white; width:60%; margin:auto; margin-top:20px;'>
-                    <div class=card-body>
-                        <h5 class=card-title>".$row['idannuncio']."</h5>
-                        <p class=card-text>Prezzo: ".$row['prezzo'].".</p>
-                        <p class=card-text>Immagine: ".$row['immagine'].".</p>
-                        <p class=card-text>Descrizione: ".$row['descrizione'].".</p>
-
-                    </div>
+                <div class=card mb-3 style='border:1px white solid; color:white; width:60%; padding:1%; margin:auto; margin-top:20px; background-color: rgb(51, 58, 65);'>
+                    <div class=row>
+                        <div class=col-md-3>
+                            <img src=uploads/".$row['immagine']." class=card-img-top style='height:30vh; border-radius:0; border:1px white solid'>
+                        </div>
+                        <div class=col-md-9>
+                            <div class=card-body>
+                                <table class=table style='width:100%; color:white;'>
+                                    <tr style='background-color:white; color:rgb(51, 58, 65);'>
+                                        <td>ID</td>
+                                        <td>".$row['idannuncio']."</td>
+                                    </tr>
+                                    <tr>
+                                    <td>PREZZO</td>
+                                        <td>".$row['prezzo']."</td>
+                                    </tr>
+                                    <tr>
+                                    <td>DESCRIZIONE</td>
+                                        <td>".$row['descrizione']."</td>
+                                    </tr>
+                                </table>
+                            </div>
+                        </div>
+                    </div>        
                 </div>
                 ";
-  
-        
             }
 
             $conn->close();
 ?>
 
 </body>
-
 </html>
